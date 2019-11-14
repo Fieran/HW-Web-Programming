de94e5ff1d56d535059f9225b763a1c41c3b1049
bn24-git <55922810+bn24-git@users.noreply.github.com>
2019-11-14 15:39:18 +0000

    Add files via upload
    added comments

--- Practice JavaScript/GoodGame/public/js/game.js
+++ Practice JavaScript/GoodGame/public/js/game.js
@@ -1,8 +1,10 @@
 var config = {
   type: Phaser.AUTO,
   parent: 'phaser-example',
-  width: 800,
-  height: 600,
+  //width of game window curreently set to width of browser
+  width: window.innerWidth,
+  //height of game window currently set to height of browser
+  height: window.innerHeight,
   physics: {
     default: 'arcade',
     arcade: {
@@ -16,15 +18,15 @@ var config = {
     update: update
   } 
 };
-
+//creates the game configuration
 var game = new Phaser.Game(config);
-
+//loads the images for the game
 function preload() {
   this.load.image('ship', 'assets/spaceShips_001.png');
   this.load.image('otherPlayer', 'assets/enemyBlack5.png');
   this.load.image('star', 'assets/star_gold.png');
 }
-
+//creates the match
 function create() {
   var self = this;
   this.socket = io();
@@ -38,35 +40,43 @@ function create() {
       }
     });
   });
+  //gets the info of the new player that joined
   this.socket.on('newPlayer', function (playerInfo) {
     addOtherPlayers(self, playerInfo);
   });
+  //deletes the player once they disconnect from the session
   this.socket.on('disconnect', function (playerId) {
+	  //updates for other player that, that player is gone
     self.otherPlayers.getChildren().forEach(function (otherPlayer) {
       if (playerId === otherPlayer.playerId) {
         otherPlayer.destroy();
       }
     });
   });
+  //updates the position of the player to other players once they move
   this.socket.on('playerMoved', function (playerInfo) {
     self.otherPlayers.getChildren().forEach(function (otherPlayer) {
       if (playerInfo.playerId === otherPlayer.playerId) {
         otherPlayer.setRotation(playerInfo.rotation);
+		//the rotation of the player
         otherPlayer.setPosition(playerInfo.x, playerInfo.y);
       }
     });
   });
   this.cursors = this.input.keyboard.createCursorKeys();
-
+//scoreboaard
+//blue scoreboard text
   this.blueScoreText = this.add.text(16, 16, '', { fontSize: '32px', fill: '#0000FF' });
+  //red scoreboard text
   this.redScoreText = this.add.text(584, 16, '', { fontSize: '32px', fill: '#FF0000' });
-  
+  //sets the scores on the score board
   this.socket.on('scoreUpdate', function (scores) {
     self.blueScoreText.setText('Blue: ' + scores.blue);
     self.redScoreText.setText('Red: ' + scores.red);
   });
-
+	//the random location of the collected item in the game
   this.socket.on('starLocation', function (starLocation) {
+	  //once the item is collected destroy the original item and spawn a new one at random coordinates
     if (self.star) self.star.destroy();
     self.star = self.physics.add.image(starLocation.x, starLocation.y, 'star');
     self.physics.add.overlap(self.ship, self.star, function () {
@@ -74,40 +84,48 @@ function create() {
     }, null, self);
   });
 }
-
+//create a player
 function addPlayer(self, playerInfo) {
+	//adds the image to the player from the assets folder
   self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+  //tints the players colour depending on what team they are
   if (playerInfo.team === 'blue') {
+	  //blue tint
     self.ship.setTint(0x0000ff);
   } else {
+	  //red tint
     self.ship.setTint(0xff0000);
   }
+  //creates the 'ice slide' effect on the player
   self.ship.setDrag(100);
   self.ship.setAngularDrag(100);
   self.ship.setMaxVelocity(200);
 }
-
+//when another player joins
 function addOtherPlayers(self, playerInfo) {
+	//change the sprite of other players relative to the player that is seeing them
   const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'otherPlayer').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
   if (playerInfo.team === 'blue') {
+	  //tints the sprite blue
     otherPlayer.setTint(0x0000ff);
-  } else {
+  } else {//tints the sprite red
     otherPlayer.setTint(0xff0000);
   }
   otherPlayer.playerId = playerInfo.playerId;
   self.otherPlayers.add(otherPlayer);
 }
-
+//update function for movement
 function update() {
   if (this.ship) {
     if (this.cursors.left.isDown) {
+		//speed of ship
       this.ship.setAngularVelocity(-150);
     } else if (this.cursors.right.isDown) {
       this.ship.setAngularVelocity(150);
-    } else {
+    } else {//speed is 0 when not pressing keys
       this.ship.setAngularVelocity(0);
     }
-  
+  //accelerate the player the longer they go in one direction
     if (this.cursors.up.isDown) {
       this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
     } else {
