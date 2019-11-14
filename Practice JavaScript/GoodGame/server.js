d44c67eb6cf9d56cd6ef8c2f6f61bb2973c077b0
bn24-git <55922810+bn24-git@users.noreply.github.com>
2019-11-14 15:37:16 +0000

    Add files via upload
    added a couple of comments and made it to work on a web server

--- Practice JavaScript/GoodGame/server.js
+++ Practice JavaScript/GoodGame/server.js
@@ -4,26 +4,29 @@ var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
 var players = {};
+//random position for item
 var star = {
   x: Math.floor(Math.random() * 700) + 50,
   y: Math.floor(Math.random() * 500) + 50
 };
+//score variables
 var scores = {
   blue: 0,
   red: 0
 };
-
+//go into the public folder
 app.use(express.static(__dirname + '/public'));
-
+//find index.html file to start the game
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
-
+//when a player connect
 io.on('connection', function (socket) {
   console.log('a user connected: ', socket.id);
   // create a new player and add it to our players object
   players[socket.id] = {
     rotation: 0,
+	//spawn the player in a random place
     x: Math.floor(Math.random() * 700) + 50,
     y: Math.floor(Math.random() * 500) + 50,
     playerId: socket.id,
@@ -57,17 +60,18 @@ io.on('connection', function (socket) {
 
   socket.on('starCollected', function () {
     if (players[socket.id].team === 'red') {
+		//add 10 points to red when red player collects item
       scores.red += 10;
     } else {
       scores.blue += 10;
-    }
+	  //add 10 points to blue when blue player collects item
+    }//reset the item to random position
     star.x = Math.floor(Math.random() * 700) + 50;
     star.y = Math.floor(Math.random() * 500) + 50;
     io.emit('starLocation', star);
     io.emit('scoreUpdate', scores);
   });
 });
-
-server.listen(8081, function () {
-  console.log(`Listening on ${server.address().port}`);
-});
+//have the port listen on the web server. if it is unavailable use port 5000 for local server
+server.listen(process.env.PORT || 5000, function () {
+  console.log("Express server listening on port %d in %s mode", this.address().port,app.settings.env)});
