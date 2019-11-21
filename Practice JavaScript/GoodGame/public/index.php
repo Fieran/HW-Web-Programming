5b1100693332dfaf3449dff2023bc0fd10506a94
ra111-git <55843317+ra111-git@users.noreply.github.com>
2019-11-21 14:31:09 +0000

    Add files via upload

---
+++ Practice JavaScript/GoodGame/public/index.php
@@ -0,0 +1,26 @@
+<?php 
+# Set page title and display header section.
+$page_title = 'Home' ;
+include ( 'includes/headerout.php' );
+?>
+<!doctype html>
+
+<html lang="en">
+<head>
+
+</head>
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item active" aria-current="page">Home</li>
+  </ol>
+</nav> 
+   <body>
+    <div class="container">
+
+
+    </div>
+  </body>
+   
+
+
+</html> 
