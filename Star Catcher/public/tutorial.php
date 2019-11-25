a6490d89ceee9884a5c34005d05e2c6dacbedff4
ra111-git <55843317+ra111-git@users.noreply.github.com>
2019-11-25 15:00:56 +0000

    Add files via upload

--- Star Catcher/public/tutorial.php
+++ Star Catcher/public/tutorial.php
@@ -16,7 +16,16 @@ include ( 'includes/headerout.php' );
 </nav> 
    <body>
     <div class="container">
-
+		
+		
+	<h1> How To Play </h1>
+		<p>Controls: To control your ship use your arrow keys to manipulate your direction</p>
+		
+	<h1> How To Win </h1>
+		<p>The premise of the game is simple, there are two teams red and blue and you will be randomly assigned to one, you will then need to pickup the star 
+		   which randomly spawns on your screen, the first team to a 1000 points wins! After which the game will reset back to zero score</p>
+	
+		
 
     </div>
   </body>
