365944ce5efd13b4f4ae573389ba55a9cc6db806
ra111-git <55843317+ra111-git@users.noreply.github.com>
2019-11-25 12:22:05 +0000

    Update game.js

--- Star Catcher/public/js/game.js
+++ Star Catcher/public/js/game.js
@@ -85,6 +85,12 @@ function create() {
   	this.socket.on('scoreUpdate', function (scores) {
     		self.blueScoreText.setText('Blue: ' + scores.blue);
     		self.redScoreText.setText('Red: ' + scores.red);
+			if (scores.red === 1000) {
+        			//Read wins
+      			}// close if
+			else if (scores.blue === 1000) {
+        			//Read wins
+      			}// close if	
   	}); // close socket
 	
 	//the random location of the collected item in the game
